b844a317fdb1426046112d67b89647b0227d1f1e
hamza-walker <hamzawalker@Hamzas-MacBook-Pro.local>
2023-07-14 12:13:07 +0200

    still need to implement the last task

--- src/main/java/com/codecool/seasonalproductdiscounter/Application.java
+++ src/main/java/com/codecool/seasonalproductdiscounter/Application.java
@@ -1,13 +1,23 @@
 package com.codecool.seasonalproductdiscounter;
 
-import com.codecool.seasonalproductdiscounter.service.discounts.*;
-import com.codecool.seasonalproductdiscounter.service.offers.*;
-import com.codecool.seasonalproductdiscounter.service.products.browser.*;
+import com.codecool.seasonalproductdiscounter.service.discounts.DiscountProvider;
+import com.codecool.seasonalproductdiscounter.service.discounts.DiscountProviderImpl;
+import com.codecool.seasonalproductdiscounter.service.discounts.DiscountService;
+import com.codecool.seasonalproductdiscounter.service.discounts.DiscountServiceImpl;
+import com.codecool.seasonalproductdiscounter.service.offers.OfferService;
+import com.codecool.seasonalproductdiscounter.service.offers.OfferServiceImpl;
+import com.codecool.seasonalproductdiscounter.service.products.browser.ProductBrowser;
+import com.codecool.seasonalproductdiscounter.service.products.browser.ProductBrowserImpl;
 import com.codecool.seasonalproductdiscounter.service.products.provider.RandomProductGenerator;
-import com.codecool.seasonalproductdiscounter.service.products.statistics.*;
-import com.codecool.seasonalproductdiscounter.service.users.*;
+import com.codecool.seasonalproductdiscounter.service.products.statistics.ProductStatistics;
+import com.codecool.seasonalproductdiscounter.service.products.statistics.ProductStatisticsImpl;
+import com.codecool.seasonalproductdiscounter.service.users.AuthenticationService;
+import com.codecool.seasonalproductdiscounter.service.users.AuthenticationServiceImpl;
 import com.codecool.seasonalproductdiscounter.ui.UiBase;
-import com.codecool.seasonalproductdiscounter.ui.factory.*;
+import com.codecool.seasonalproductdiscounter.ui.factory.OffersUiFactory;
+import com.codecool.seasonalproductdiscounter.ui.factory.ProductsUiFactory;
+import com.codecool.seasonalproductdiscounter.ui.factory.StatisticsUiFactory;
+import com.codecool.seasonalproductdiscounter.ui.factory.UiFactoryBase;
 import com.codecool.seasonalproductdiscounter.ui.selector.UiSelector;
 
 import java.util.ArrayList;
@@ -16,29 +26,32 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
-        RandomProductGenerator productProvider = new RandomProductGenerator(100, 10, 50);
-        ProductBrowser productBrowser = new ProductBrowserImpl(productProvider);
+        RandomProductGenerator productGenerator = new RandomProductGenerator(100, 10, 50);
+        ProductBrowser productBrowser = new ProductBrowserImpl(productGenerator);
         ProductStatistics productStatistics = new ProductStatisticsImpl(productBrowser);
         DiscountProvider discountProvider = new DiscountProviderImpl();
-        DiscountService discounterService = new DiscountServiceImpl(discountProvider);
-        OfferService offerService = null;
-        AuthenticationService authenticationService = null;
-        ProductsUiFactory productsUiFactory = new ProductsUiFactory(authenticationService, productBrowser);
+        DiscountService discountService = new DiscountServiceImpl(discountProvider);
+        OfferService offerService = new OfferServiceImpl(productGenerator, discountService);
+        AuthenticationService authenticationService = new AuthenticationServiceImpl();
 
+        ProductsUiFactory productsUiFactory = new ProductsUiFactory(authenticationService, productBrowser);
+        StatisticsUiFactory statisticsUiFactory = new StatisticsUiFactory(authenticationService, productStatistics);
+        OffersUiFactory offersUiFactory = new OffersUiFactory(authenticationService, offerService);
 
         List<UiFactoryBase> factories = new ArrayList<>();
         factories.add(productsUiFactory);
-
+        factories.add(statisticsUiFactory);
+        factories.add(offersUiFactory);
 
         UiSelector uiSelector = new UiSelector(factories);
         UiBase ui = uiSelector.select();
-        if (ui.authenticate())
-        {
+
+        if (ui.authenticate()) {
             ui.displayTitle();
             ui.run();
         }
+
         System.out.println("Press any key to exit.");
         new Scanner(System.in).nextLine();
     }
-
 }
--- src/main/java/com/codecool/seasonalproductdiscounter/ui/selector/UiSelector.java
+++ src/main/java/com/codecool/seasonalproductdiscounter/ui/selector/UiSelector.java
@@ -4,6 +4,7 @@ import com.codecool.seasonalproductdiscounter.ui.UiBase;
 import com.codecool.seasonalproductdiscounter.ui.factory.UiFactoryBase;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class UiSelector {
     private final List<UiFactoryBase> factories;
@@ -16,20 +17,29 @@ public class UiSelector {
         System.out.println("Welcome to Seasonal Product Discounter v3");
         displayMenu();
 
-        //...
-        return null;
+        int choice = getIntInput();
+        while (choice < 1 || choice > factories.size()) {
+            System.out.println("Invalid choice. Please select again.");
+            displayMenu();
+            choice = getIntInput();
+        }
+
+        UiFactoryBase selectedFactory = factories.get(choice - 1);
+        return selectedFactory.create();
     }
 
     private void displayMenu() {
         System.out.println("Available screens:");
-
-        //...
+        for (int i = 0; i < factories.size(); i++) {
+            UiFactoryBase factory = factories.get(i);
+            System.out.println((i + 1) + ". " + factory.getUiName());
+        }
     }
 
     private static int getIntInput() {
-
-        //...
-        return 0;
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        scanner.nextLine(); // Consume the remaining newline character
+        return choice;
     }
 }
-
